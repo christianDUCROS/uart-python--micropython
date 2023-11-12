c6b509301bcd42f4b357b64a7e182da5a7bd38d7
Christian Ducros <96232175+christianDUCROS@users.noreply.github.com>
2023-11-12 18:59:46 +0100

    Add files via upload

--- PC_serie.py
+++ PC_serie.py
@@ -9,23 +9,18 @@ if USB_serial.isOpen():
     print(USB_serial.name + ' is open…')
     print(USB_serial.get_settings())  #affichage des paramètres UART
 
-#---------------Envoi d'un nombre --------------
-nombre = int(input("donnez un nombre : "))
-USB_serial.write(nombre) #Envoi du nombre
-print('nombre envoyé :',nombre)
-
+#ser.write(b'hello') #Ecriture de « hello » sur le port
 #----------------Envoi message----------------
 message = input("Ecriture un mot : ")
-#ser.write(b'hello') #Ecriture de « hello » sur le port
-message_serie = message.encode("utf-8") #format bytes
-USB_serial.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+USB_serial.write(message_encode) #Envoi du message
 print('message envoyé :',message)
 
 #-------Envoi de variable avec indicateur-----
 variable = 45 #exemple nombre ou texte
 message  = str(variable) + 'R' #R est un indicateur
-message_serie = message.encode("utf-8") #format bytes
-USB_serial.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+USB_serial.write(message_encode) #Envoi du message
 print('message envoyé :',message)
 
 #-------Envoi des plusieurs variables ---------
@@ -33,15 +28,20 @@ variable1 = 45 #exemple nombre ou texte
 variable2 = 78
 variable3 = 235
 message  = str(variable1) + ' ' + str(variable2) + ' '+str(variable3)
-message_serie = message.encode("utf-8") #format bytes
-USB_serial.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+USB_serial.write(message_encode) #Envoi du message
 print('message envoyé :',message)
 
 #----------------reception  message----------------
 if USB_serial.in_waiting > 0: 
     #Lire le message reçu
-    message = USB_serial.readline().decode("utf-8")
+    message = USB_serial.readline()
+    message_decode= message.decode("utf-8")
     USB_serial.flush() #efface le buffer
-    message = message.split(" ")# espace comme separateur
-    print(message) #liste
-    print(message[0])
+    print('message reçu :',message_decode) # debug
+    
+    #-------reception de plusieurs variables séparées par espace---------
+    message_decode = message_decode.split(" ") # espace comme separateur
+    print(message_decode) # liste
+    print(message_decode[0])
+    
--- RP2_serie.py
+++ RP2_serie.py
@@ -3,27 +3,22 @@ uart1 = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
 
 #----------------Envoi message----------------
 message = "information1"
-message_serie = message.encode("utf-8") #format bytes
-uart1.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+uart1.write(message_encode) #Envoi du message
 print('message envoyé :',message)
-
-#---------------Envoi d'un nombre --------------
-nombre = 45
-uart1.write(nombre) #Envoi du nombre
-print('nombre envoyé :',nombre)
 
 #-------Envoi de variable avec indicateur-----
 variable = 45 #exemple nombre ou texte
-message_serie = message.encode("utf-8") #format bytes
-uart1.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+uart1.write(message_encode) #Envoi du message
 print('message envoyé :',message)
 
 #-------Envoi des plusieurs variables ---------
 variable1 = 45 #exemple nombre ou texte
 variable2 = 78
 variable3 = 235
-message_serie = message.encode("utf-8") #format bytes
-uart1.write(message_serie) #Envoi du message
+message_encode = message.encode("utf-8") #format bytes
+uart1.write(message_encode) #Envoi du message
 print('message envoyé :',message)
 
 
@@ -31,15 +26,18 @@ print('message envoyé :',message)
 #----------------reception  message----------------
 if uart1.any() > 0: 
     #Lire le message reçu
-    message = uart1.read().decode("utf-8")
-    message = message.split(" ")# espace comme separateur
-    print(message) #liste
+    message = uart1.read()
+    message_decode= message.decode("utf-8")
+    print('message reçu :',message_decode) # debug
+    
+    #-------reception de plusieurs variables séparées par espace---------
+    message_decode = message_decode.split(" ") # espace comme separateur
+    print(message) # liste
     print(message[0])
-
-
 
 '''
 #---test reception nombre avant conversion en nombre---
-if message[0].isdigit(): 
-    valeur1 =  int(message[0]   
+if message_decode[0].isdigit(): 
+    valeur1 =  int(message_decode[0])   
+
 '''
